4c356f25140c22dc33ea9744d19e0648f55f0745
owlinux1000 <encry1024@gmail.com>
2023-06-12 10:23:53 +0900

    add .config/fish

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,42 @@
+if status is-interactive
+    
+    # Ignore greeting
+    set fish_greeting
+
+    set -x EDITOR "/usr/local/bin/emacs -nw"
+
+    # pyenv
+    set -x PYENV_ROOT $HOME/.pyenv
+    set -x PATH $PYENV_ROOT/bin $PATH
+    pyenv init - | source
+
+    # rbenv
+    set -x RBENV_ROOT $HOME/.rbenv
+    set -x PATH $RBENV_ROOT/bin $PATH
+    rbenv init - | source
+
+    # goenv
+    set -x GOENV_ROOT $HOME/.goenv
+    set -x PATH $GOENV_ROOT/bin $PATH
+    goenv init - | source
+    
+    # tfenv
+    set -x PATH $HOME/.tfenv/bin $PATH
+    
+    # rust
+    set -x PATH $HOME/.cargo/bin $PATH
+
+    
+    # abbr
+    abbr -a -- emacs 'emacs -nw'
+    abbr -a -- k 'kubectl'
+    abbr -a -- d 'docker'
+    abbr -a -- dc 'docker compose'
+
+    # alias
+    alias pbcopy='xsel -b -i'
+
+    # chezmoi
+    
+    
+end
